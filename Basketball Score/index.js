274aa61e50c578a01d00556a42d0ac8844966f00
hamdiali <129667371+hamdiali29@users.noreply.github.com>
2023-11-11 01:51:03 +0000

    Add files via upload

---
+++ Basketball Score/index.js
@@ -0,0 +1,31 @@
+let homeBtnEl = document.getElementById("homebtn-el")
+let homebtn = 0
+
+function hmonepoint(){
+    homebtn += 1
+    homeBtnEl.textContent = homebtn
+}
+function hmtwopoint(){
+    homebtn += 2
+    homeBtnEl.textContent = homebtn
+}
+function hmthreepoint(){
+    homebtn += 3
+    homeBtnEl.textContent = homebtn
+}
+
+let guestBtnEl = document.getElementById("guestbtn-el")
+let guestbtn = 0
+
+function gstonepoint(){
+    guestbtn += 1
+    guestBtnEl.textContent = guestbtn
+}
+function gsttwopoint(){
+    guestbtn += 2
+    guestBtnEl.textContent = guestbtn
+}
+function gstthreepoint(){
+    guestbtn += 3
+    guestBtnEl.textContent = guestbtn
+}
